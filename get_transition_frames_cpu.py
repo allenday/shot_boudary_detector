8f3d8ef98ff8338a2fb977fb3633ed2d8f361dec
Allen Day <allenday@allenday.com>
2021-06-16 21:00:51 +0800

    cleanup

--- get_transition_frames_cpu.py
+++ get_transition_frames_cpu.py
@@ -44,13 +44,12 @@ f = open(text_file, 'w+')
 with Bar('extracting frames', max=n_frames) as bar:
         for j, frame in enumerate(vid.iter_frames()):
 
-                if j % 1000 == 0:
-                        print('processed frames: ' + str(j) + '/' + str(n_frames))
                 frame_path = frames_path + 'frame_' + str(j+1) + '.jpg'
                 im = Image.fromarray(frame)
                 im.save(frame_path, 'JPEG')            
                 f.write(frame_path + '\n')    
-        bar.next()
+                bar.next()
+
 
 print('frame decomposition complete !!! ')
 
